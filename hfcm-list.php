a829e300a763a76ce024c967c90a3eab51a50d86
abhishek jain <abhishek.jain@t9l.com>
2016-06-23 02:27:52 +0530

    Changes based on Feedback

--- hfcm-list.php
+++ hfcm-list.php
@@ -4,12 +4,18 @@ function hfcm_list() {
     global $wpdb;
     $table_name = $wpdb->prefix . "hfcm_scripts";
     $rows = $wpdb->get_results("SELECT * from $table_name");
+    $activecount = $wpdb->get_results("SELECT COUNT(*) as count from $table_name where status = 'active' ");
+    $inactivecount = $wpdb->get_results("SELECT COUNT(*) as count from $table_name where status = 'inactive'");
     ?>
-    <link type="text/css" href="<?php echo plugins_url( 'assets/css/', __FILE__ ); ?>style-admin.css" rel="stylesheet" />
+    <link type="text/css" href="<?php echo plugins_url('assets/css/', __FILE__); ?>style-admin.css" rel="stylesheet" />
     <div class="wrap">
         <h1>Snippets 
             <a href="<?php echo admin_url('admin.php?page=hfcm-create'); ?>" class="page-title-action">Add New Snippet</a>
         </h1>
+        <ul class="subsubsub">
+            <li class="all">Active <span class="count">(<?php echo $activecount[0]->count; ?>)</span> |</li>
+            <li class="publish">Inactive <span class="count">(<?php echo $inactivecount[0]->count; ?>)</span></li>
+        </ul>
         <table class='wp-list-table widefat fixed striped posts'>
             <thead>
                 <tr>
