458c70c24e5bdca2512bb271914e3772a2f6b720
Francisco Mejia <franmejia0@gmail.com>
2016-08-12 10:36:29 -0400

    Improve toggle button's AJAX
    * IMPROVED: unsing recommended AJAX method for toggling snippets on/off

--- js/toggle.js
+++ js/toggle.js
@@ -1,16 +1,17 @@
 jQuery('.nnr-switch input').click( function()  {
 	var t = jQuery( this ),
 		togvalue = t.is( ':checked' ) ? 'on' : 'off',
-		scriptid = t.data( 'id' );
-										
-	jQuery.ajax({
-		url: hfcm_ajax.url, 
-		data: {
-			page: 'hfcm-request-handler',
+		scriptid = t.data( 'id' ),
+		data = {
+			action: 'hfcm-request',
 			toggle: true,
 			id: scriptid,
 			togvalue: togvalue,
 			security: hfcm_ajax.security
-		}
-	});
+		};
+
+	jQuery.post(
+		ajaxurl,
+		data
+	);
 });
