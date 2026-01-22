a1e2ab1e71ab6bfcf8f6b2d64ec460e7242d46b0
abhij89 <abhij89@gmail.com>
2026-01-22 19:40:10 +0530

    = 1.1.44 = 2026-01-22 * ADDED: Compatibility with WordPress 6.9

--- 99robots-header-footer-code-manager.php
+++ 99robots-header-footer-code-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Header Footer Code Manager
  * Plugin URI: https://draftpress.com/products
  * Description: Header Footer Code Manager is a quick and simple way for you to add tracking code snippets, conversion pixels, or other scripts required by third party services for analytics, tracking, marketing, or chat functions. Used by 700,000+ sites. For detailed documentation, please visit the plugin’s <a href="https://draftpress.com/"> official page</a>.
- * Version: 1.1.43
+ * Version: 1.1.44
  * Requires at least: 4.9
  * Requires PHP: 5.6.20
  * Author: DraftPress
